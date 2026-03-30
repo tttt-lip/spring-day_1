395a4099e72f9f921534de63898f69662feaa4c5
Teshale C. <teshale.cella@liparipeople.com>
2026-03-30 20:24:09 +0200

    fix(LipariBankApplication): aggiunto @ComponentScan per permettere di fare scan di altri bean al di fuori della cartella che si trova @SpringApplicaiton

--- src/main/java/com/lipari/bank/LipariBankApplication.java
+++ src/main/java/com/lipari/bank/LipariBankApplication.java
@@ -1,9 +1,14 @@
 package com.lipari.bank;
 
+import com.lipari.bank.shared.config.LipariBankProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.lipari.bank", "com.liparibank.lifecycle"})
+@EnableConfigurationProperties(LipariBankProperties.class)
 public class LipariBankApplication {
 
     public static void main(String[] args) {
